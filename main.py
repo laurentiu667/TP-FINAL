29d2d9ae26b9c6784014f5f36f4c8dd5b830241b
Yacine-Alexis <yacinealexis@outlook.fr>
2023-11-28 09:43:57 -0500

    12

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import time
 import random
 import helper as hp
 
@@ -15,39 +14,43 @@ class Creep:
         self.valeur = valeur
         self.couleur = couleur
         self.taille = taille
+        self.chemin_courant = 0
         self.x = chemin[0][0] * self.parent.cell_size
         self.y = chemin[0][1] * self.parent.cell_size
-        self.x_cible = chemin[0][2] * self.parent.cell_size
-        self.y_cible = chemin[0][3] * self.parent.cell_size
+        self.x_cible = (chemin[0][2] * self.parent.cell_size) - self.parent.cell_size / 2
+        self.y_cible = (chemin[0][3] * self.parent.cell_size) - self.parent.cell_size / 2
         self.angle = hp.Helper.calcAngle(self.x,self.y,self.x_cible,self.y_cible)
-        self.chemin_courant = 0
 
     def deplacer(self):
         self.x, self.y = hp.Helper.getAngledPoint(self.angle,self.vitesse,self.x,self.y)
         dist = hp.Helper.calcDistance(self.x,self.y,self.x_cible,self.y_cible)
         if dist <= self.vitesse:
             self.chemin_courant += 1
-            chemin = self.parent.chemin(self.chemin_courant)
-            self.x = chemin[0][0] * self.parent.cell_size
-            self.y = chemin[0][1] * self.parent.cell_size
-            self.x_cible = chemin[0][2] * self.parent.cell_size
-            self.y_cible = chemin[0][3] * self.parent.cell_size
-            self.angle = hp.Helper.calcAngle(self.x, self.y, self.x_cible, self.y_cible)
+            if self.chemin_courant < len(self.parent.chemin):
+                chemin = self.parent.chemin(self.chemin_courant)
+                self.x = chemin[0][0] * self.parent.cell_size
+                self.y = chemin[0][1] * self.parent.cell_size
+                self.x_cible = (chemin[0][2] * self.parent.cell_size) - self.parent.cell_size / 2
+                self.y_cible = (chemin[0][3] * self.parent.cell_size) - self.parent.cell_size / 2
+                self.angle = hp.Helper.calcAngle(self.x, self.y, self.x_cible, self.y_cible)
+            else:
+               pass
+               # tuer creep + chatelin
 
 class Modele:
     def __init__(self, root=None):
         self.cell_size = 40
-        self.temps_avant_vague = 5000
+        self.temps_avant_niveau = 100
         self.timer_id = None
-        self.root = root
+        #self.root = root
         self.vies_chateau = 20
         self.joueur = Joueur(argent_initial=100)
         self.creeps = []
         self.creeps_en_jeu = []
-        self.positions_pixels = []
-        self.niveau_actuel = 1
+        #self.positions_pixels = []
+        self.niveau_actuel = 0
         self.delai_depart = 0
-        self.delai_depart_max = 3
+        self.delai_depart_max = 20
 
         self.chemin = [
             [(5, 0, 7, 16), "white"],
@@ -59,8 +62,8 @@ class Modele:
             [(19, 10, 21, 16), "white"],
             [(21, 14, 29, 16), "white"]
         ]
-
-        self.creer_creeps()
+        self.creer_niveau()
+        #self.creer_creeps()
 
         self.chateau = [
             [(29, 13, 31, 16), "grey"],
@@ -76,8 +79,10 @@ class Modele:
                 creep = self.creeps.pop()
                 self.creeps_en_jeu.append(creep)
                 self.delai_depart = self.delai_depart_max
+            else:
+                self.delai_depart -= 1
 
-    def creer_creeps(self):
+    def creer_niveau(self):
         for i in range(20):
             vitesse = 1
             valeur = 10
@@ -87,24 +92,25 @@ class Modele:
             self.creeps.append(creep)
 
 class Vue:
-    def __init__(self, root, modele):
-        self.root = root
-        self.root.title("Tower Defense")
-        self.root.geometry("1920x1080")
+    def __init__(self, parent, modele):
+        self.parent = parent
+        self.modele = modele
 
-        self.container = tk.Frame(root)
+        #self.root.geometry("1920x1080")
+        self.root = tk.Tk()
+        self.root.title("Tower Defense")
+        self.container = tk.Frame(self.root)
         self.container.pack()
 
-        self.container_palette_infos = tk.Frame(root)
+        self.container_palette_infos = tk.Frame(self.root)
         self.canvas = tk.Canvas(self.container, width=1320, height=960, bg="black")
         self.canvas.pack()
 
-        self.modele = modele
         self.dessiner_jeu()
 
-        self.timer_running = True
-        self.start_time = time.time()
-        self.update_timer()
+        #self.timer_running = True
+        #self.start_time = time.time()
+        #self.update_timer()
 
         self.vies_label = tk.Label(self.container, text=f"Vies restantes : {self.modele.vies_chateau}",
                                    font=("Helvetica", 16),
@@ -132,63 +138,42 @@ class Vue:
             y2 *= self.modele.cell_size
             self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="")
 
-    def dessiner_creeps(self):
-        x_start = 5.5 * self.modele.cell_size
-        y = 2 * self.modele.cell_size
+    def afficher_jeu(self):
+        self.canvas.delete("dynamique")
+        #x_start = 5.5 * self.modele.cell_size
+        #y = 2 * self.modele.cell_size
 
-        for creep in self.modele.creeps:
-            width = self.modele.cell_size
-            height = self.modele.cell_size
-            self.canvas.create_oval(x_start, y, x_start + width, y + height, fill="red", outline="white")
-            x_start += 0 * width
-
-    def dessiner_creep(self, position):
-        width = self.modele.cell_size
-        height = self.modele.cell_size
-        self.canvas.create_oval(position[0], position[1],
-                                position[0] + width, position[1] + height,
-                                fill="red", outline="white")
-
-    def update_timer(self):
-        if self.timer_running:
-            elapsed_time = int(time.time() - self.start_time)
-            remaining_time = max(5 - elapsed_time, 0)
-
-            self.canvas.delete("timer")
-
-            minutes = remaining_time // 60
-            seconds = remaining_time % 60
-
-            self.canvas.create_text(100, 700,
-                                    text=f"Chrono:\n {minutes:02d}:{seconds:02d}",
-                                    font=("Helvetica", 16),
-                                    fill="white", tags="timer")
-
-            if remaining_time == 0:
-                self.dessiner_creeps()
-
-            self.root.after(1000, self.update_timer)
+        for creep in self.modele.creeps_en_jeu:
+            largeur = self.modele.cell_size / 2
+            hauteur = self.modele.cell_size / 2
+            #self.canvas.create_oval(x_start, y, x_start + width, y + height, fill="red", outline="white")
+            self.canvas.create_oval(creep.x - largeur, creep.y - hauteur, creep.x + largeur, creep.y + hauteur, fill="red", outline="white",
+                                    tags=("dynamique", "creep"))
 
     def ignore_timer(self, event):
         self.timer_running = False
         self.canvas.delete("timer")
-        self.dessiner_creeps()
 
     def update_vies_label(self):
         self.vies_label.config(text=f"Vies restantes : {self.modele.vies_chateau}")
 
 class Controleur:
     def __init__(self):
-        self.root = tk.Tk()
-        self.modele = Modele(self.root)
-        self.vue = Vue(self.root, self.modele)
+        self.no_iteration = 0
+        self.modele = Modele(self)
+        self.vue = Vue(self, self.modele)
+        self.vue.root.after(100, self.boucler_jeu)
+        self.vue.root.mainloop()
 
-    def demarrer(self):
-        self.root.mainloop()
+    def boucler_jeu(self):
+        self.no_iteration += 1
+        self.modele.jouer_coup()
+        self.vue.afficher_jeu() # affiche uniquement les choes dinq;iaue du jeu
+        self.vue.root.after(50, self.boucler_jeu)
 
 def main():
     c = Controleur()
-    c.demarrer()
+    #c.demarrer()
 
 if __name__ == "__main__":
     main()
