406481a80f23eb3b6f9aedf7826595cca7b93304
laurentiu667 <dilionlaur12@gmail.com>
2023-11-28 22:05:41 -0500

    LA BONNE VERSION DE JEUDI 28 NOV

--- main.py
+++ main.py
@@ -15,65 +15,67 @@ class Creep:
         self.couleur = couleur
         self.taille = taille
         self.chemin_courant = 0
-        self.x = chemin[0][0] * self.parent.cell_size
-        self.y = chemin[0][1] * self.parent.cell_size
-        self.x_cible = (chemin[0][2] * self.parent.cell_size) - self.parent.cell_size / 2
-        self.y_cible = (chemin[0][3] * self.parent.cell_size) - self.parent.cell_size / 2
-        self.angle = hp.Helper.calcAngle(self.x,self.y,self.x_cible,self.y_cible)
+        self.x = chemin[0][0]
+        self.y = chemin[0][1]
+        self.x_cible = chemin[0][2]
+        self.y_cible = chemin[0][3]
+        self.angle = hp.Helper.calcAngle(self.x, self.y, self.x_cible, self.y_cible)
 
     def deplacer(self):
-        self.x, self.y = hp.Helper.getAngledPoint(self.angle,self.vitesse,self.x,self.y)
-        dist = hp.Helper.calcDistance(self.x,self.y,self.x_cible,self.y_cible)
+        self.x, self.y = hp.Helper.getAngledPoint(self.angle, self.vitesse, self.x, self.y)
+        dist = hp.Helper.calcDistance(self.x, self.y, self.x_cible, self.y_cible)
         if dist <= self.vitesse:
             self.chemin_courant += 1
             if self.chemin_courant < len(self.parent.chemin):
-                chemin = self.parent.chemin(self.chemin_courant)
-                self.x = chemin[0][0] * self.parent.cell_size
-                self.y = chemin[0][1] * self.parent.cell_size
-                self.x_cible = (chemin[0][2] * self.parent.cell_size) - self.parent.cell_size / 2
-                self.y_cible = (chemin[0][3] * self.parent.cell_size) - self.parent.cell_size / 2
+                chemin = self.parent.chemin[self.chemin_courant]
+                self.x = chemin[0][0]
+                self.y = chemin[0][1]
+                self.x_cible = chemin[0][2]
+                self.y_cible = chemin[0][3]
                 self.angle = hp.Helper.calcAngle(self.x, self.y, self.x_cible, self.y_cible)
             else:
-               pass
-               # tuer creep + chatelin
+               self.parent.supprimer_creep(self)
 
 class Modele:
-    def __init__(self, root=None):
-        self.cell_size = 40
+    def __init__(self):
         self.temps_avant_niveau = 100
         self.timer_id = None
-        #self.root = root
         self.vies_chateau = 20
         self.joueur = Joueur(argent_initial=100)
         self.creeps = []
         self.creeps_en_jeu = []
-        #self.positions_pixels = []
         self.niveau_actuel = 0
         self.delai_depart = 0
-        self.delai_depart_max = 20
+        self.delai_depart_max = 25
 
         self.chemin = [
-            [(5, 0, 7, 16), "white"],
-            [(7, 14, 11, 16), "white"],
-            [(11, 3, 13, 16), "white"],
-            [(13, 3, 30, 5), "white"],
-            [(28, 5, 30, 10), "white"],
-            [(19, 8, 28, 10), "white"],
-            [(19, 10, 21, 16), "white"],
-            [(21, 14, 29, 16), "white"]
+            [(200,    0,  200, 640), "white"],
+            [(200,  640,  440, 640), "white"],
+            [(440,  640,  440, 120), "white"],
+            [(440,  120, 1100, 120), "white"],
+            [(1100, 120, 1100, 400), "white"],
+            [(1100, 400,  760, 400), "white"],
+            [(760,  400,  760, 640), "white"],
+            [(760,  640, 1100, 640), "white"]
         ]
+
         self.creer_niveau()
-        #self.creer_creeps()
 
         self.chateau = [
-            [(29, 13, 31, 16), "grey"],
-            [(28, 12, 29, 13), "grey"],
-            [(31, 12, 32, 13), "grey"]
+            [(1100, 550, 1100, 670), "grey"],
+            [(1100, 550, 1050, 550), "grey"],
+            [(1150, 550, 1100, 550), "grey"]
         ]
 
+    def supprimer_creep(self):
+        self.vies_chateau -= 1
+        self.creeps_en_jeu.remove(self)
+        # add creep dans une liste de creep_mort[]
+        # puis lorsque les creeps ont fini de bouger, enlever creep de la liste de creep en jeu
+
     def jouer_coup(self):
-        for i in self.creeps_en_jeu:
-            i.deplacer()
+        for creep in self.creeps_en_jeu:
+            creep.deplacer()
         if self.creeps:
             if self.delai_depart == 0:
                 creep = self.creeps.pop()
@@ -84,7 +86,7 @@ class Modele:
 
     def creer_niveau(self):
         for i in range(20):
-            vitesse = 1
+            vitesse = 15
             valeur = 10
             couleur = "red"
             taille = int(random.randrange(80, 120) / 2)
@@ -96,63 +98,41 @@ class Vue:
         self.parent = parent
         self.modele = modele
 
-        #self.root.geometry("1920x1080")
         self.root = tk.Tk()
         self.root.title("Tower Defense")
         self.container = tk.Frame(self.root)
         self.container.pack()
 
         self.container_palette_infos = tk.Frame(self.root)
-        self.canvas = tk.Canvas(self.container, width=1320, height=960, bg="black")
+        self.canvas = tk.Canvas(self.container, width=1200, height=960, bg="black")
         self.canvas.pack()
 
         self.dessiner_jeu()
-
-        #self.timer_running = True
-        #self.start_time = time.time()
-        #self.update_timer()
 
         self.vies_label = tk.Label(self.container, text=f"Vies restantes : {self.modele.vies_chateau}",
                                    font=("Helvetica", 16),
                                    fg="white", bg="black")
         self.vies_label.pack(side=tk.RIGHT)
 
-        self.root.bind("<space>", self.ignore_timer)
-
     def dessiner_jeu(self):
         for p in self.modele.chemin:
             coords, color = p
             x1, y1, x2, y2 = coords
-            x1 *= self.modele.cell_size
-            y1 *= self.modele.cell_size
-            x2 *= self.modele.cell_size
-            y2 *= self.modele.cell_size
-            self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="")
+            self.canvas.create_line(x1, y1, x2, y2, fill=color, width="75", capstyle="round")
 
         for c in self.modele.chateau:
             coords, color = c
             x1, y1, x2, y2 = coords
-            x1 *= self.modele.cell_size
-            y1 *= self.modele.cell_size
-            x2 *= self.modele.cell_size
-            y2 *= self.modele.cell_size
-            self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="")
+            self.canvas.create_line(x1, y1, x2, y2, fill=color,width="50")
 
     def afficher_jeu(self):
         self.canvas.delete("dynamique")
-        #x_start = 5.5 * self.modele.cell_size
-        #y = 2 * self.modele.cell_size
 
         for creep in self.modele.creeps_en_jeu:
-            largeur = self.modele.cell_size / 2
-            hauteur = self.modele.cell_size / 2
-            #self.canvas.create_oval(x_start, y, x_start + width, y + height, fill="red", outline="white")
+            largeur = 20
+            hauteur = 20
             self.canvas.create_oval(creep.x - largeur, creep.y - hauteur, creep.x + largeur, creep.y + hauteur, fill="red", outline="white",
                                     tags=("dynamique", "creep"))
-
-    def ignore_timer(self, event):
-        self.timer_running = False
-        self.canvas.delete("timer")
 
     def update_vies_label(self):
         self.vies_label.config(text=f"Vies restantes : {self.modele.vies_chateau}")
@@ -160,7 +140,7 @@ class Vue:
 class Controleur:
     def __init__(self):
         self.no_iteration = 0
-        self.modele = Modele(self)
+        self.modele = Modele()
         self.vue = Vue(self, self.modele)
         self.vue.root.after(100, self.boucler_jeu)
         self.vue.root.mainloop()
@@ -168,12 +148,11 @@ class Controleur:
     def boucler_jeu(self):
         self.no_iteration += 1
         self.modele.jouer_coup()
-        self.vue.afficher_jeu() # affiche uniquement les choes dinq;iaue du jeu
+        self.vue.afficher_jeu() # affiche uniquement les choses dynamique du jeu
         self.vue.root.after(50, self.boucler_jeu)
 
 def main():
-    c = Controleur()
-    #c.demarrer()
+    Controleur()
 
 if __name__ == "__main__":
     main()
